542bdb1fd61fd9babba48533f91ffe20c71a74cc
Blaire Baker <bakerblaire@gmail.com>
2020-11-04 18:31:26 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def find_item_by_name_in_collection(name, collection)
   collection.find do |index|
     index[:item] == name
@@ -9,6 +11,7 @@ def consolidate_cart(cart)
   cart.each do |index|
     if find_item_by_name_in_collection(index[:item], new_cart) == nil
       new_cart.push(index)
+      binding.pry
       new_cart[index][:count] = 1
     else
       new_cart[index][:count] += 1
