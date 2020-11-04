260a298f53e1933640c2b06cf0724710e7181b87
Blaire Baker <bakerblaire@gmail.com>
2020-11-04 18:17:26 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,6 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  collection.find do |index|
+    index[:item] == name
 
 end
 
