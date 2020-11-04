2099870b4f7e8842506ba78f23a3a1fafe9ca953
Blaire Baker <bakerblaire@gmail.com>
2020-11-04 18:33:25 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -10,9 +10,9 @@ def consolidate_cart(cart)
   new_cart = []
   cart.each do |index|
     if find_item_by_name_in_collection(index[:item], new_cart) == nil
+      index[:count] = 1
       new_cart.push(index)
       binding.pry
-      new_cart[index][:count] = 1
     else
       new_cart[index][:count] += 1
     end
